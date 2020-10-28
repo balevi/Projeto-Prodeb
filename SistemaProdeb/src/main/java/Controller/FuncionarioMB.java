68bc7a74b7c54723d26bcacfb3891c349865bd93
Levi Almeida <levialmeida1994@hotmail.com>
2020-10-28 17:25:56 -0300

    att

--- SistemaProdeb/src/main/java/Controller/FuncionarioMB.java
+++ SistemaProdeb/src/main/java/Controller/FuncionarioMB.java
@@ -1,49 +1,57 @@
 package Controller;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ManagedBean;
+//import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+//import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
 
 import Model.Funcionario;
 import DAO.FuncionarioDao;
 
-@ManagedBean(name="funci")
+@Named("funci")
 @RequestScoped
-public class FuncionarioMB {
+public class FuncionarioMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Inject
 	Funcionario funcionario;
+	
 	List<Funcionario> funcionarios;
 	FuncionarioDao funcionarioDao;
 	
-public FuncionarioMB () {
+	public FuncionarioMB () {
 		funcionarioDao = new FuncionarioDao();
 		funcionario = new Funcionario();
 	}
 
-public String salvar() {
-	funcionarioDao.salvar(funcionario);
-	return "index";
-}
+	public String salvar() {
+		funcionarioDao.salvar(funcionario);
+		return "CadastroPessoa";
+	}
 
-public List<Funcionario> getFuncionarios() {
-	funcionarios = funcionarioDao.ListaTodos();
-	return funcionarios;
-}
+	public List<Funcionario> getFuncionarios() {
+		funcionarios = funcionarioDao.ListaTodos();
+		return funcionarios;
+	}
 
-public void setFuncionarios(List<Funcionario> funcionarios) {
-	this.funcionarios = funcionarios;
-}
-
-
-public Funcionario getFuncionario() {
-	return funcionario;
-}
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 
 
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
 
-public void setFuncionario(Funcionario funcionario) {
-	this.funcionario = funcionario;
-}
+
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;	
+	}
 
 
 
